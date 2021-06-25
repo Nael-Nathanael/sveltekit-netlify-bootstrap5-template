bb9fa6cd143fbf9431ed50a5343a0db832b2edcc
Nathanael <nael.nathanael71@gmail.com>
2021-06-25 10:56:25 +0800

    init: bootstrap 5 template

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
-import netlify from '@sveltejs/adapter-netlify'
+import netlify from '@sveltejs/adapter-netlify';
+import { resolve } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: netlify()
+		adapter: netlify(),
+		vite: {
+			resolve: {
+				alias: {
+					$components: resolve('src/components/')
+				}
+			}
+		}
 	}
 };
 
